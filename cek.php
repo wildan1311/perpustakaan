7ee474a85ce12cca1132f469ae9cfecfa645bf9c
wildan1311 <wmaaf1311@gmail.com>
2022-05-26 09:40:57 +0700

    login

---
+++ cek.php
@@ -0,0 +1,24 @@
+<?php
+    @include 'koneksi.php';
+
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $login = mysqli_query($koneksi, "Select username, password, multiuser from user where username=$username and password=$password");
+    
+    if(mysqli_num_rows($login)>0){
+        session_start(); 
+        $_SESSION['username'] = $username;
+        $_SESSION['password'] = $password;
+        if(mysqli_fetch_assoc($login)['level']=='admin'){
+            $_SESSION['level'] = 'admin';
+            header("location:admin.php");
+        }else if(mysqli_fetch_assoc($login)['level']=='anggota'){
+            $_SESSION['level'] = 'anggota';
+            header("location:anggota.php");
+        }
+    }else{
+        //tampilkan error
+        header("location:login.php?pesan=gagal");
+    }
+?>
