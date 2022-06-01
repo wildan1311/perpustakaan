720ea9ab695a1b8f5671290a34c89f336a24c6d3
hezbims <hezbisulaiman@gmail.com>
2022-06-01 17:48:56 +0800

    Merapikan repository

--- koneksi.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $koneksi = mysqli_connect('localhost','root', '', 'perpus');
-
-    if($koneksi-> connect_errno){
-        echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
-        exit();
-    }
-?>
