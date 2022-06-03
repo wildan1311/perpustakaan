d5c4d1a763f6f7f8dfb6559536d3412d8ebb83c8
wildan1311 <wmaaf1311@gmail.com>
2022-06-03 16:28:07 +0700

    ganti href side nav dorm pinjam

--- sidenav.php
+++ sidenav.php
@@ -34,7 +34,7 @@
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link text-white " href="login.php">
+          <a class="nav-link text-white " href="form_pinjam.php">
             <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
               <i class="material-icons opacity-10">receipt_long</i>
             </div>
