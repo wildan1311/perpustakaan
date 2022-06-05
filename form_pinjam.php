d5d72bb7d4898494434486827ca634a408e47148
hezbims <hezbisulaiman@gmail.com>
2022-06-05 16:42:48 +0800

    Membetulkan syntax di form_pinjam.php

--- form_pinjam.php
+++ form_pinjam.php
@@ -10,7 +10,7 @@
   </title>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
   <!--     Fonts and icons     -->
-  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
+  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900%7CRoboto+Slab:400,700" />
   <!-- Nucleo Icons -->
   <link href="./assets/css/nucleo-icons.css" rel="stylesheet" />
   <link href="./assets/css/nucleo-svg.css" rel="stylesheet" />
@@ -45,46 +45,47 @@
               </div>
             </div>
             <div class="card-body px-0 pb-2">
-            <form id="inputform" method="POST">
-                <div class="mb-3 ms-5 me-5 col-4">
-                  <label for="id_buku" class="form-label" value="nrp" >Masukkan NRP</label>
-                  <input type="text" class="form-control" name="nrp" id="nrp">
-                </div>
+              <form id="inputform" method="POST">
+
                   <div class="mb-3 ms-5 me-5 col-4">
-                    <label for="id_buku" class="form-label" value="NRP">Masukkan ID Buku</label>
-                    <input type="text" placeholder="Search id Buku" class="form-control d-inline" name="id_buku" onchange="show()">
+                    <label for="nrp" class="form-label">Masukkan NRP</label>
+                    <input type="text" class="form-control" name="nrp" id="nrp">
                   </div>
-                  <div class="table-responsive p-0 mb-3 ms-5 me-5" id="show">
-                    <table class="table align-items-center mb-0">
-                      <thead>
-                        <tr>
-                          <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2 text-center">Id Buku</th>
-                          <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Judul Buku</th>
-                          <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Pengarang</th>
-                          <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Penerbit</th>
-                          <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Action</th>
-                        </tr>
-                      </thead>
-                      <tbody id="isi">
-                        
-                      </tbody>
-                    </table>
-                  </div>                
-                <input type="button" class="btn btn-primary ms-5 me-5 float-end" name="submit" onclick="kerjakan()" value="SUBMIT"></input>
-                <!-- <input type="checkbox" id="bismillah"> -->
-                </div>
-            </form>
+
+                    <div class="mb-3 ms-5 me-5 col-4">
+                      <label for="id_buku" class="form-label">Masukkan ID Buku</label>
+                      <input type="text" placeholder="Search id Buku" class="form-control d-inline" name="id_buku" onchange="show()" id="id_buku">
+                    </div>
+
+                    <div class="table-responsive p-0 mb-3 ms-5 me-5" id="show">
+                      <table class="table align-items-center mb-0">
+                        <thead>
+                          <tr>
+                            <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2 text-center">Id Buku</th>
+                            <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Judul Buku</th>
+                            <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Pengarang</th>
+                            <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Penerbit</th>
+                            <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Action</th>
+                          </tr>
+                        </thead>
+                        <tbody id="isi">
+                          
+                        </tbody>
+                      </table>
+                    </div>
+
+                  <input type="button" class="btn btn-primary ms-5 me-5 float-end" name="submit" onclick="kerjakan()" value="SUBMIT">
+              </form>
+            </div>
             <!-- end form -->
             <!-- show notif -->
               <button class="btn bg-gradient-success w-100 mb-0 toast-btn" style="display: none;" id="success" type="button" data-target="successToast">Success</button>
               <button class="btn bg-gradient-danger w-100 mb-0 toast-btn" style="display: none;" id="failed" type="button" data-target="dangerToast">Failed</button>
               <button class="btn bg-gradient-danger w-100 mb-0 toast-btn" style="display: none;" id="bahaya" type="button" data-target="warningToast">Failed</button>
-            </div>
-        </div>
-            </div>
           </div>
         </div>
       </div>
+    </div>
   <!-- NOTIF -->
                   
   <div class="position-fixed bottom-1 end-1 z-index-2">
@@ -145,86 +146,8 @@
           </div>
         </div>
       </div>
-      <!-- notifikasi -->
   </main>
-  <!-- <div class="fixed-plugin">
-    <a class="fixed-plugin-button text-dark position-fixed px-3 py-2">
-      <i class="material-icons py-2">settings</i>
-    </a>
-    <div class="card shadow-lg">
-      <div class="card-header pb-0 pt-3">
-        <div class="float-start">
-          <h5 class="mt-3 mb-0">Material UI Configurator</h5>
-          <p>See our dashboard options.</p>
-        </div>
-        <div class="float-end mt-4">
-          <button class="btn btn-link text-dark p-0 fixed-plugin-close-button">
-            <i class="material-icons">clear</i>
-          </button>
-        </div> -->
-        <!-- End Toggle Button -->
-      <!-- </div>
-      <hr class="horizontal dark my-1"> -->
-      <!-- <div class="card-body pt-sm-3 pt-0"> -->
-        <!-- Sidebar Backgrounds -->
-        <!-- <div>
-          <h6 class="mb-0">Sidebar Colors</h6>
-        </div>
-        <a href="javascript:void(0)" class="switch-trigger background-color">
-          <div class="badge-colors my-2 text-start">
-            <span class="badge filter bg-gradient-primary active" data-color="primary" onclick="sidebarColor(this)"></span>
-            <span class="badge filter bg-gradient-dark" data-color="dark" onclick="sidebarColor(this)"></span>
-            <span class="badge filter bg-gradient-info" data-color="info" onclick="sidebarColor(this)"></span>
-            <span class="badge filter bg-gradient-success" data-color="success" onclick="sidebarColor(this)"></span>
-            <span class="badge filter bg-gradient-warning" data-color="warning" onclick="sidebarColor(this)"></span>
-            <span class="badge filter bg-gradient-danger" data-color="danger" onclick="sidebarColor(this)"></span>
-          </div>
-        </a> -->
-        <!-- Sidenav Type -->
-        <!-- <div class="mt-3">
-          <h6 class="mb-0">Sidenav Type</h6>
-          <p class="text-sm">Choose between 2 different sidenav types.</p>
-        </div>
-        <div class="d-flex">
-          <button class="btn bg-gradient-dark px-3 mb-2 active" data-class="bg-gradient-dark" onclick="sidebarType(this)">Dark</button>
-          <button class="btn bg-gradient-dark px-3 mb-2 ms-2" data-class="bg-transparent" onclick="sidebarType(this)">Transparent</button>
-          <button class="btn bg-gradient-dark px-3 mb-2 ms-2" data-class="bg-white" onclick="sidebarType(this)">White</button>
-        </div>
-        <p class="text-sm d-xl-none d-block mt-2">You can change the sidenav type just on desktop view.</p> -->
-        <!-- Navbar Fixed -->
-        <!-- <div class="mt-3 d-flex">
-          <h6 class="mb-0">Navbar Fixed</h6>
-          <div class="form-check form-switch ps-0 ms-auto my-auto">
-            <input class="form-check-input mt-1 ms-auto" type="checkbox" id="navbarFixed" onclick="navbarFixed(this)">
-          </div>
-        </div>
-        <hr class="horizontal dark my-3">
-        <div class="mt-2 d-flex">
-          <h6 class="mb-0">Light / Dark</h6>
-          <div class="form-check form-switch ps-0 ms-auto my-auto">
-            <input class="form-check-input mt-1 ms-auto" type="checkbox" id="dark-version" onclick="darkMode(this)">
-          </div>
-        </div>
-        <hr class="horizontal dark my-sm-4">
-        <a class="btn bg-gradient-info w-100" href="https://www.creative-tim.com/product/material-dashboard">Free Download</a>
-        <a class="btn btn-outline-dark w-100" href="https://www.creative-tim.com/learning-lab/bootstrap/overview/material-dashboard">View documentation</a>
-        <div class="w-100 text-center">
-          <a class="github-button" href="https://github.com/creativetimofficial/material-dashboard" data-icon="octicon-star" data-size="large" data-show-count="true" aria-label="Star creativetimofficial/material-dashboard on GitHub">Star</a>
-          <h6 class="mt-3">Thank you for sharing!</h6>
-          <a href="https://twitter.com/intent/tweet?text=Check%20Material%20UI%20Dashboard%20made%20by%20%40CreativeTim%20%23webdesign%20%23dashboard%20%23bootstrap5&amp;url=https%3A%2F%2Fwww.creative-tim.com%2Fproduct%2Fsoft-ui-dashboard" class="btn btn-dark mb-0 me-2" target="_blank">
-            <i class="fab fa-twitter me-1" aria-hidden="true"></i> Tweet
-          </a>
-          <a href="https://www.facebook.com/sharer/sharer.php?u=https://www.creative-tim.com/product/material-dashboard" class="btn btn-dark mb-0 me-2" target="_blank">
-            <i class="fab fa-facebook-square me-1" aria-hidden="true"></i> Share
-          </a>
-        </div>
-      </div>
-    </div>
-  </div> -->
-
-  <!-- NOTIFIKASI -->
   
-
 
   <!--   Core JS Files   -->
   <script src="../assets/js/core/popper.min.js"></script>
@@ -246,10 +169,7 @@
   <script src="../assets/js/material-dashboard.min.js?v=3.0.2"></script>
 
   <!-- button to show notif -->
-  
-</body>
-
-<script>
+  <script>
   function kerjakan(){
     $.ajax({
       type: 'POST',
@@ -287,6 +207,7 @@
       }
     })
   }
-</script>
+  </script>
+</body>
 
 </html>
