667ea16c687e84de2734bd77a5f3a078172c12f6
hezbims <hezbisulaiman@gmail.com>
2022-11-20 21:14:08 +0800

    Menambahkan procedure di ajax_form_pinjam

--- ajax_form_pinjam.php
+++ ajax_form_pinjam.php
@@ -6,6 +6,21 @@
     $tomorrow = date('Y-m-d',strtotime($now1 . "+10 days"));
     
     @include 'koneksi.php';
+    ociexecute(ociparse($koneksi , "
+      create or replace PROCEDURE peminjaman
+      (
+      idbuku_ IN VARCHAR2,
+      id_anggota_ IN VARCHAR2,
+      tgl_pinjam_ IN date,
+      tgl_kembali_ IN date,
+      tabel_ IN VARCHAR2
+      ) AS
+      BEGIN
+      insert into transaksi(id_buku, id_anggota, tgl_pinjam, tgl_kembali) values(idbuku_,id_anggota_, tgl_pinjam_, tgl_kembali_);
+      commit;
+      END peminjaman;
+    "));
+    
     $sql = "select u.nrp, b.id_buku from pengguna u, buku b where u.nrp='$id_anggota' and b.id_buku = '$id_buku'";
     $sql = ociparse($koneksi, $sql);
     ociexecute($sql);
